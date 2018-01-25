131206e11fb426546e8bd4248120e9b004c3ebf3
Percy Maldonado Quispe <percy.maldonado@ucsp.edu.pe>
2018-01-25 10:26:58 -0500

    Frame StringGrid Data

---
+++ framestrgrid.pas
@@ -0,0 +1,27 @@
+unit FrameStrGrid;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Grids;
+
+type
+
+  { TFrame2 }
+
+  TFrame2 = class(TFrame)
+    StringGrid1: TStringGrid;
+  private
+
+  public
+
+  end;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
