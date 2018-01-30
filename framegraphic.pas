e7078a63cc6b3a9a9f8904990f25353ab67e6534
Percy Maldonado Quispe <percy.maldonado@ucsp.edu.pe>
2018-01-30 18:32:51 -0500

    Frame Para Graficar [Plot Points-Function]

--- framegraphic.pas
+++ framegraphic.pas
@@ -6,16 +6,25 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, TAGraph, TASeries, Forms, Controls, StdCtrls,
-  ParseMath;
+  ParseMath, Func, Math, TAChartUtils;
 
 type
+
+  { TFrame1 }
+
   TFrame1 = class(TFrame)
     Chart1: TChart;
+    AreaDraw: TAreaSeries;
+    GraficaIII: TLineSeries;
+    GraficaII: TLineSeries;
     GraficaI: TLineSeries;
   public
     MParse: TParseMath;
-    procedure Plotear(fn: string; xmin,xmax,h: real);
+    procedure Plotear(fn: string; xmin,xmax,h: real; state: boolean);
+    procedure PlotearIntersection(f1,f2: string; xmin,xmax,h: real);
     procedure CreateParse;
+    procedure PlotearPoints(MS: TList; Stick: boolean);
+    procedure PlotearPointFunct(MS: TList);
     function Func(x: real; fn: string): real;
   private
 end;
@@ -35,20 +44,74 @@ begin
   Result:= MParse.Evaluate();
 end;
 
-procedure TFrame1.Plotear(fn: string; xmin,xmax,h: real);
+procedure TFrame1.Plotear(fn: string; xmin,xmax,h: real; state: boolean);
 var
-  x: Real;
+  x,y,xmna,xmxa: Real;
+begin
+  x:= xmin;
+  xmna:= xmin+ShiftArea;
+  xmxa:= xmax-ShiftArea;
+  CreateParse;
+  GraficaI.Clear;
+  AreaDraw.clear;
+  if not state then begin
+    with GraficaI do repeat
+      AddXY(x,Func(x,fn));
+      x:= x+h
+    until(x>=xmax);
+  end
+  else begin
+    with GraficaI do repeat
+      y:= Func(x,fn);
+      AddXY(x,y);
+      if(x>=xmna) and (x<=xmxa) then
+        AreaDraw.AddXY(x,y);
+      x:= x+h
+    until(x>=xmax);
+  end;
+end;
+
+procedure TFrame1.PlotearIntersection(f1,f2: string; xmin,xmax,h: real);
+var
+  x,y: Real;
 begin
   x:= xmin;
   CreateParse;
   GraficaI.Clear;
+  GraficaII.Clear;
   with GraficaI do repeat
-    AddXY(x,Func(x,fn));
+    AddXY(x,Func(x,f1));
+    GraficaII.AddXY(x,Func(x,f2));
     x:= x+h
   until(x>=xmax);
+end;
+
+procedure TFrame1.PlotearPoints(MS: TList; Stick: boolean);
+var
+  i: integer;
+  TM: TMPoint;
+begin
+  GraficaIII.ShowPoints:= True;
+  if Stick then
+    GraficaIII.Marks.Style:= smsValue;
+  for i:=0 to MS.Count-1 do begin
+    TM:= TMPoint(MS[i]);
+    GraficaIII.AddXY(TM.x,TM.y);
+    GraficaIII.AddXY(NaN,NaN);
+  end;
+end;
+
+procedure TFrame1.PlotearPointFunct(MS: TList);
+var
+  i: integer;
+  TM: TMPoint;
+begin
+  for i:=0 to MS.Count-1 do begin
+    TM:= TMPoint(MS[i]);
+    GraficaII.AddXY(TM.x,TM.y);
+  end;
 end;
 
 {$R *.lfm}
 
 end.
-
